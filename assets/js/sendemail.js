7807dd4e929305ce8b2d18facc8f9f1a780715b2
joffy69 <jcwgibbs@gmail.com>
2021-08-01 16:55:08 +0000

    further work on emails

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,15 @@
-console.log('hello');
+function sendMail(contactForm){
+    emailjs.send('service_d6m0fqy','template_6g0ew0b',{
+        from_name: contactForm.name.value,
+        from_email: contactForm.emailaddress.value,
+        project_request: contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log('success', response);
+        },
+        function(error){
+            console.log("Failed", error);
+        });
+        return false;
+}
